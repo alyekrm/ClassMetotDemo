1e8a5da2e3a439658e30d0dd0975ca0a80811caa
aly ekrm <alyekrm@hotmail.com>
2021-01-17 23:32:19 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        static public void MusteriEkle(ref int counter)
+        {
+            Console.WriteLine("Sicil No Giriniz:");
+            string Sicil = Console.ReadLine();
+            Console.WriteLine("Müşteri adını giriniz:");
+            string Ad = Console.ReadLine();
+            Console.WriteLine("Müşteri soyadını giriniz");
+            string SoyAd = Console.ReadLine();
+            counter = counter + 1;
+            Musteri YeniMusteri = new Musteri() { Id = counter, Adi = Ad, Soyadi = SoyAd };
+            Console.WriteLine(YeniMusteri.Adi+" "+YeniMusteri.Soyadi + " Başarıyla eklendi");
+        }
+        static public void MusteriSil(Musteri[] Musteriler)
+        {
+            Console.WriteLine("Silinecek müşterinin Id'sini giriniz");
+            int Id = Convert.ToInt16(Console.ReadLine());
+            foreach (var musteri in Musteriler)
+            {
+                if (musteri.Id == Id)
+                {
+                    Console.WriteLine(musteri.Adi);
+                    Console.WriteLine(musteri.Soyadi);
+                    Console.WriteLine(musteri.Id);
+                    musteri.Adi = "Silindi";
+                    musteri.Soyadi = "Silindi";
+                    Console.WriteLine("Id numarası:" + Id + " olan müşteri silindi");
+                }
+            }
+        }
+        static public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Adı Soyadı Müşteri Sistem Numarası");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine(musteri.Adi+" "+musteri.Soyadi+" "+musteri.Id);
+                Console.WriteLine("***********");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            int counter = 1;
+            Musteri Musteri1 = new Musteri();
+            {
+                Musteri1.Id = 1;
+                Musteri1.Adi = "Ali";
+                Musteri1.Soyadi = "Veli";
+            }
+            Musteri[] musteriler = new Musteri[] { Musteri1 };
+            Menu();
+            string Secim = Console.ReadLine();
+            if (Secim == "1")
+            {
+                MusteriManager.MusteriListele(musteriler);
+                Yeni();
+            }
+            else if (Secim=="2")
+            {
+                MusteriManager.MusteriEkle(ref counter);
+                Yeni();
+            }
+            else if (Secim == "3")
+            {
+                MusteriManager.MusteriSil(musteriler);
+                Yeni();
+            }
+            else
+            {
+                return;
+            }
+        }
+        static void Menu()
+        {
+            Console.WriteLine("Yapmak istediğiniz işlemi seçinizi");
+            Console.WriteLine("Müşterileri listelemek için 1'e,");
+            Console.WriteLine("Yeni müşteri eklemek için 2'ye,");
+            Console.WriteLine("Ekli müşteriyi silmek için 3'e ");
+            Console.WriteLine("Çıkmak için 4'e basınız");
+        }
+        static void Yeni()
+        {
+            Console.WriteLine("Yeni bir işlem yapmak ister misiniz?");
+            string Cevap = Console.ReadLine();
+            switch (Cevap)
+            {
+                case "evet":
+                    Program.Main();
+                    break;
+                case "hayır":
+                    break;
+                default:
+                    Console.WriteLine("Lütfen cevabınızı evet yada hayır olarak giriniz");
+                    Yeni();
+                    break;
+            }
+        }
+    }
+}
